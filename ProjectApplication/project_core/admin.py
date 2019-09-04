30044253bc01cc36b80b3497ea56cb2c95163a97
Jen Thomas <jenny_t152@yahoo.co.uk>
2019-09-04 15:07:35 +0100

    Created models and admin models for date and call. Includes migrations.

--- ProjectApplication/project_core/admin.py
+++ ProjectApplication/project_core/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+import project_core.models
+
 
 # Register your models here.
+
+
+class DateAdmin(admin.ModelAdmin):
+    list_display = ('notable_date', 'date')
+    ordering = ['notable_date', 'date']
+
+
+class CallAdmin(admin.ModelAdmin):
+    list_display = ('long_name', 'short_name', 'description', 'dates_list')
+    ordering = ['long_name', 'short_name', 'description']
+
+    def dates_list(self, obj):
+        dates = obj.dates.all()
+
+        return ", ".join(["{}".format(date) for date in dates])
+
+
+admin.site.register(project_core.models.Date, DateAdmin)
+admin.site.register(project_core.models.Call, CallAdmin)
--- ProjectApplication/project_core/models.py
+++ ProjectApplication/project_core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Date(models.Model):
+    """Model containing a list of notable dates that are used throughout the application."""
+    notable_date = models.CharField(help_text="Description of notable date",max_length=128, null=False)
+    date = models.DateTimeField(help_text="Date and time of notable date",max_length=64, null=False)
+
+    def __str__(self):
+        return "{} - {}".format(self.notable_date, self.date)
+
+
+class Call(models.Model):
+    """Description of calls."""
+    long_name = models.CharField(help_text="Full name of the call", max_length=128, blank=False, null=False)
+    short_name = models.CharField(help_text="Short name or acronym of the call", max_length=64, blank=True, null=True)
+    description = models.TextField(help_text="Description of the call that can be used to distinguish it from others", blank=False, null=False)
+    dates = models.ManyToManyField(Date, help_text="Notable date related to the call", blank=False)
+
+    def __str__(self):
+        return "{}".format(self.long_name)
+
+
