9f3241ead7e5e35d530f2ed0ace2cdc00035a7fb
Carles Pina i Estany <carles@pina.cat>
2019-09-04 11:57:56 +0100

    Reads Mysql configuration from /etc/mysql/projects.cnf

--- ProjectApplication/ProjectApplication/settings.py
+++ ProjectApplication/ProjectApplication/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ProjectApplication.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/projects.cnf',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
