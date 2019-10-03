3e9d97af25f16113c8b3399d5e4739e5962c3056
Carles Pina i Estany <carles@pina.cat>
2019-10-03 10:29:36 +0300

    Change url and text of the proposals

--- ProjectApplication/project_core/urls.py
+++ ProjectApplication/project_core/urls.py
@@ -5,7 +5,7 @@ from .views import Homepage, CallsList, ProposalView, ProposalsList, ProposalTha
 urlpatterns = [
     path('', Homepage.as_view(), name='homepage'),
     path('calls/', CallsList.as_view(), name='calls-list'),
-    path('proposals', ProposalsList.as_view(), name='proposals-list'),
+    path('internal/proposals', ProposalsList.as_view(), name='proposals-list'),
     path('proposal/add/', ProposalView.as_view(), name='proposal-add'),
     path('proposal/<uuid:uuid>/', ProposalView.as_view(), name='proposal-update'),
     path('proposal/thank-you/<uuid:uuid>/', ProposalThankYouView.as_view(), name='proposal-thank-you'),
