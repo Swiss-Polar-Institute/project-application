922f7abf80c70c9af2b39998bee988ffb20155b8
Carles Pina i Estany <carles@pina.cat>
2019-10-09 09:13:25 +0300

    Converts one more form to crispy form

--- ProjectApplication/project_core/forms/question.py
+++ ProjectApplication/project_core/forms/question.py
@@ -1,11 +1,15 @@
 from django import forms
 
 from ..models import TemplateQuestion
+from crispy_forms.helper import FormHelper
 
 
 class QuestionForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper(self)
+        self.helper.form_tag = False
 
     class Meta:
         model = TemplateQuestion
