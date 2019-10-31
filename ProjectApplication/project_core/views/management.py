06eeb4a2305b08bf24bc82ac3d9190fcb275d7f2
Carles Pina i Estany <carles@pina.cat>
2019-10-31 09:08:29 +0100

    Makes titles in management look consistent (always Project Management - {{ subtitle }})

--- ProjectApplication/project_core/views/management.py
+++ ProjectApplication/project_core/views/management.py
@@ -35,6 +35,10 @@ class Homepage(TemplateView):
     def get(self, request, *args, **kwargs):
         context = super().get_context_data(**kwargs)
 
+        context['active_section'] = 'home'
+        context['active_subsection'] = 'home'
+        context['sidebar_template'] = 'management/_sidebar-homepage.tmpl'
+
         return render(request, 'management/homepage.tmpl', context)
 
 
