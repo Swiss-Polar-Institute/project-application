30bc6eb45ecef695270c6d3415701a067fb4a9a9
Carles Pina i Estany <carles@pina.cat>
2019-11-07 20:55:19 +0100

    Fixes typo

--- ProjectApplication/ProjectApplication/settings.py
+++ ProjectApplication/ProjectApplication/settings.py
@@ -186,4 +186,4 @@ DEMO_MANAGEMENT_PASSWORD = os.environ['DEMO_MANAGEMENT_PASSWORD']
 SECRET_KEY = os.environ.get('SECRET_KEY', 'shfl_mdb^frjpk8@5@fsl(qm0^u0+--m6_x28lgbil*m&#+rvq')
 
 if 'ALLOWED_HOST_1' in os.environ:
-    ALLOWED_HOSTS.append(os.environ['ALLOWED_HOSTS_1'])
+    ALLOWED_HOSTS.append(os.environ['ALLOWED_HOST_1'])
