81e11034dede4a4325b7b2eb6b56308c026a3f38
Carles Pina i Estany <carles@pina.cat>
2019-12-04 16:49:23 +0000

    Now it's able to serve staging and live.

--- launch-production.sh
+++ launch-production.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker-compose -f docker-compose.yml down
-docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose.yml up --build -d
--- launch-staging.sh
+++ launch-staging.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker-compose -f docker-compose.yml -f docker-compose.staging.yml down
-docker-compose -f docker-compose.yml -f docker-compose.staging.yml up -d
+docker-compose -f docker-compose.yml -f docker-compose.staging.yml up --build -d
