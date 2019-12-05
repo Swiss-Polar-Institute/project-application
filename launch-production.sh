e43fc19b3a2df11ef2a52194dc0a3583f4dd1d57
Carles Pina i Estany <carles@pina.cat>
2019-12-05 09:19:31 +0000

    Separates docker-compose for production and staging

--- launch-production.sh
+++ launch-production.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yml down
-docker-compose -f docker-compose.yml up --build -d
+docker-compose -f docker-compose.yml -f docker-compose.live.yml down
+docker-compose -f docker-compose.yml -f docker-compose.live.yml up --build -d
