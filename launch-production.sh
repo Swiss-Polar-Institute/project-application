293a01d1bfd80714e4931ebca52920cd7fb2c344
Carles Pina i Estany <carles@pina.cat>
2019-12-05 09:36:02 +0000

    Renames file

--- launch-production.sh
+++ launch-production.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yml -f docker-compose.live.yml down
-docker-compose -f docker-compose.yml -f docker-compose.live.yml up --build -d
+docker-compose -f docker-compose.yml -f docker-compose.production.yml down
+docker-compose -f docker-compose.yml -f docker-compose.production.yml up --build -d
