49b0716a3fac7887dfc3b1355d1e830ed1b846f6
Carles Pina i Estany <carles@pina.cat>
2019-12-10 16:52:37 +0100

    Adds comment

--- launch-development.sh
+++ launch-development.sh
@@ -2,3 +2,5 @@
 
 docker-compose -f docker-compose.yml -f docker-compose.staging.yml -f docker-compose.development.yml down
 docker-compose -f docker-compose.yml -f docker-compose.staging.yml -f docker-compose.development.yml up --build
+
+echo "All going well it should be available in http://localhost:1235"
