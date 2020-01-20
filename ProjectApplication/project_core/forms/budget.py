ecf8bd3ee14a078976e2fc76cf7d9319bb1d09d2
Carles Pina i Estany <carles@pina.cat>
2020-01-20 15:50:58 +0100

    Avoids adding NoneType to 0 when the amount is empty.

--- ProjectApplication/project_core/forms/budget.py
+++ ProjectApplication/project_core/forms/budget.py
@@ -61,7 +61,7 @@ class BudgetItemForm(forms.Form):
             self.add_error('amount', 'Please write a number (do not use thousands separator if you have a problem) {}'.format(category))
             amount = 0
         else:
-            amount = cleaned_data['amount']
+            amount = cleaned_data['amount'] or 0
 
         details = cleaned_data['details'] or ''
 
