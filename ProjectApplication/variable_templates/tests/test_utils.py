4bf51acdeabfa7580abbcd40fb67f290e193f0d0
Carles Pina i Estany <carles@pina.cat>
2020-01-22 13:26:55 +0100

    Improves unit test

--- ProjectApplication/variable_templates/tests/test_utils.py
+++ ProjectApplication/variable_templates/tests/test_utils.py
@@ -1,9 +1,19 @@
+from collections import OrderedDict
+
+from django import forms
 from django.test import TestCase
 
 from project_core.tests import database_population as project_core_database_population
 from . import database_population
 from .. import utils
 
+
+# class Field:
+#     def __init__(self):
+#         self.help_text = None
+#         self.label = None
+#
+#     pass
 
 class UtilsTest(TestCase):
     def setUp(self):
@@ -12,3 +22,17 @@ class UtilsTest(TestCase):
 
     def test_get_template_value(self):
         utils.get_template_value('activity', self._call)
+
+    def test_apply_templates(self):
+        title_field = forms.CharField(label='Name', help_text='Name of the {{ activity }}')
+        keywords_field = forms.CharField(label='Keywords', help_text='List the keywords')
+
+        fields = OrderedDict({'title': title_field,
+                              'keywords': keywords_field
+                              })
+
+        self.assertIn('{{ activity }}', fields['title'].help_text)
+
+        utils.apply_templates(fields, self._call)
+
+        self.assertNotIn('{{ activity }}', fields['title'].help_text)
