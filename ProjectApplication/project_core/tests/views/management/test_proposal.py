e6a24250b4146941800ae4b87e5be8c9696a3de8
Carles Pina i Estany <carles@pina.cat>
2020-01-23 12:49:34 +0100

    Adds unit test for CSV of proposals

--- ProjectApplication/project_core/tests/views/management/test_proposal.py
+++ ProjectApplication/project_core/tests/views/management/test_proposal.py
@@ -1,12 +1,16 @@
+from django.test import Client
 from django.test import TestCase
+from django.urls import reverse
 
 from ... import database_population
 from ....views.management import proposal
 
 
-class CallFormTest(TestCase):
+class ManagementProposalTest(TestCase):
     def setUp(self):
         self._call = database_population.create_call()
+        self._proposal = database_population.create_proposal()
+        database_population.create_management_user()
 
     def test_create_file_name(self):
         filename = proposal.create_file_name('this-is-{}-something-{}.csv', self._call.id)
@@ -15,4 +19,15 @@ class CallFormTest(TestCase):
         self._call.short_name = 'GLACE'
         self._call.save()
         filename = proposal.create_file_name('this-is-{}-something-{}.csv', self._call.id)
-        self.assertRegex(filename, '^this-is-GLACE-[0-9]{8}-[0-9]{6}\.csv$')
+        self.assertRegex(filename, '^this-is-GLACE-something-[0-9]{8}-[0-9]{6}\.csv$')
+
+    def test_proposals_export_csv_summary(self):
+        c = Client()
+
+        login = c.login(username='unittest', password='12345')
+        self.assertTrue(login)
+
+        response = c.get(
+            reverse('management-export-proposals-csv-summary-call', args=[str(self._proposal.call.id)]))
+
+        self.assertContains(response, 'A test proposal')
