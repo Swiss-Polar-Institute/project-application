30fc468d1f48d67069ab52a366c4587506dd7a9d
Carles Pina i Estany <carles@pina.cat>
2020-02-11 13:00:01 +0000

    Adds new models into the admin

--- ProjectApplication/comments/admin.py
+++ ProjectApplication/comments/admin.py
@@ -4,32 +4,40 @@ import comments.models
 
 
 class ProposalCommentAdmin(admin.ModelAdmin):
-    list_display = ('proposal', 'text', 'created_by', 'created_on', 'modified_by', 'modified_on',)
+    list_display = ('proposal', 'text', 'created_by', 'created_on',)
     ordering = ['proposal', 'text', 'created_by', 'created_on', 'modified_by', 'modified_on', ]
 
 
 class CallCommentAdmin(admin.ModelAdmin):
-    list_display = ('call', 'text', 'created_by', 'created_on', 'modified_by', 'modified_on',)
+    list_display = ('call', 'text', 'created_by', 'created_on',)
     ordering = ['call', 'text', 'created_by', 'created_on', 'modified_by', 'modified_on', ]
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('category',)
+    list_display = ('category', 'created_by', 'created_on',)
     ordering = ['category']
 
 
 class ProposalCommentCategoryAdmin(admin.ModelAdmin):
-    list_display = ('category',)
+    list_display = ('category', 'created_by', 'created_on',)
     ordering = ['category', ]
 
 
 class ProposalAttachmentCategoryAdmin(admin.ModelAdmin):
-    list_display = ('category',)
+    list_display = ('category', 'created_by', 'created_on',)
     ordering = ['category', ]
 
 
 class ProposalAttachmentAdmin(admin.ModelAdmin):
-    list_display = ('proposal', 'file')
+    list_display = ('proposal', 'file', 'created_by', 'created_on',)
+
+
+class CallAttachmentAdmin(admin.ModelAdmin):
+    list_display = ('call', 'file', 'created_by', 'created_on',)
+
+
+class CallAttachmentCategoryAdmin(admin.ModelAdmin):
+    list_display = ('category', 'created_by', 'created_on',)
 
 
 admin.site.register(comments.models.ProposalComment, ProposalCommentAdmin)
@@ -38,3 +46,5 @@ admin.site.register(comments.models.Category, CategoryAdmin)
 admin.site.register(comments.models.ProposalCommentCategory, ProposalCommentCategoryAdmin)
 admin.site.register(comments.models.ProposalAttachmentCategory, ProposalAttachmentCategoryAdmin)
 admin.site.register(comments.models.ProposalAttachment, ProposalAttachmentAdmin)
+admin.site.register(comments.models.CallAttachment, CallAttachmentAdmin)
+admin.site.register(comments.models.CallAttachmentCategory, CallAttachmentCategoryAdmin)
