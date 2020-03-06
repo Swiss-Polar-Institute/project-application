43a134eac709d694cc86fd15f4258437973b2f8b
Carles Pina i Estany <carles@pina.cat>
2020-03-06 16:44:54 +0100

    Basic unit test for one method in the Proposal model

---
+++ ProjectApplication/project_core/tests/models/test_proposal.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from django.test import TestCase
+
+from evaluation.models import CallEvaluation
+from project_core.models import Proposal
+from project_core.tests import database_population
+
+
+class ProposalModelTest(TestCase):
+    def setUp(self):
+        self._call = database_population.create_call()
+
+    def test_proposal_can_call_evaluation_be_visualised(self):
+        proposal = Proposal(call=self._call)
+        self.assertFalse(proposal.can_call_evaluation_be_visualised())
+
+        call_evaluation = CallEvaluation(call=self._call, panel_date=datetime.now())
+        call_evaluation.save()
+
+        self.assertTrue(proposal.can_call_evaluation_be_visualised())
