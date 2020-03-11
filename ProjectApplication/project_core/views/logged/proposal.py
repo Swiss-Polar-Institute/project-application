b293d872f430eb612b150c7d3ba3d88f89219c5f
Carles Pina i Estany <carles@pina.cat>
2020-03-11 13:12:45 +0100

    Adds breadcrumb on the proposal detail.
    (TODO: I need to review what the reviewers see after this change)

--- ProjectApplication/project_core/views/logged/proposal.py
+++ ProjectApplication/project_core/views/logged/proposal.py
@@ -1,7 +1,5 @@
-from django.contrib import messages
-from django.core.exceptions import PermissionDenied
-from django.shortcuts import redirect, render
-from django.urls import reverse
+from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
 from django.utils import timezone
 from django.views.generic import TemplateView
 
@@ -121,9 +119,10 @@ class ProposalDetailView(AbstractProposalDetailView):
 
     template = 'logged/proposal-detail.tmpl'
 
-    extra_context = {'active_section': 'proposals',
+    extra_context = {'active_section': 'lists',
                      'active_subsection': 'proposal-list',
-                     'sidebar_template': 'logged/_sidebar-proposals.tmpl'}
+                     'sidebar_template': 'logged/_sidebar-lists.tmpl',
+                     'breadcrumb': [{'name': 'Lists', 'url': reverse_lazy('logged-lists')}, {'name': 'Proposals'}]}
 
 
 class ProposalView(AbstractProposalView):
