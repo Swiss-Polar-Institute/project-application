509b31639c40746a719636af4455cf097fa9b0d1
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-03-17 16:20:02 +0000

    project field duration_months removed from admin.

--- ProjectApplication/project_core/admin.py
+++ ProjectApplication/project_core/admin.py
@@ -299,9 +299,9 @@ class ProposalQAFileAdmin(admin.ModelAdmin):
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'uuid', 'keywords_list', 'geographical_area_list', 'location', 'start_date',
-                    'end_date', 'duration_months', 'principal_investigator', 'call', 'proposal', 'overarching_project',
+                    'end_date', 'principal_investigator', 'call', 'proposal', 'overarching_project',
                     'allocated_budget', 'status', 'abortion_reason', 'created_on', 'modified_on',)
-    ordering = ['title', 'uuid', 'location', 'start_date', 'end_date', 'duration_months', 'principal_investigator',
+    ordering = ['title', 'uuid', 'location', 'start_date', 'end_date', 'principal_investigator',
                 'call', 'proposal', 'allocated_budget', 'status', 'abortion_reason',]
 
     def keywords_list(self, obj):
