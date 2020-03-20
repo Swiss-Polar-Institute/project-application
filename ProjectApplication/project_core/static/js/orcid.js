6faa6c6be0a022bfb4a7dd3e1b5a2fcebbed96b4
Carles Pina i Estany <carles@pina.cat>
2020-03-20 16:21:37 +0100

    Orcid ID test implementation - it works but changes in the model and the proposal form are needed.

--- ProjectApplication/project_core/static/js/orcid.js
+++ ProjectApplication/project_core/static/js/orcid.js
@@ -1,3 +1,24 @@
-$.getJSON("https://pub.orcid.org/v3.0/0000-0003-3120-9475", function(data) {
-    alert(data);
-});
+function getOrcid() {
+    $.ajax({
+        url: "https://pub.orcid.org/v3.0/0000-0003-3120-9475",
+        type: "GET",
+        dataType: "JSON",
+        beforeSend: setHeader,
+        success: orcid_success,
+        error: orcid_error
+    });
+
+    function orcid_success(orcid_record) {
+        let family_name = orcid_record['person']['name']['family-name'].value;
+        let given_names = orcid_record['person']['name']['given-names'].value;
+        alert('orcid success: ' + given_names + " " + family_name);
+    }
+
+    function orcid_error() {
+        alert('orcid failure');
+    }
+
+    function setHeader(request) {
+        request.setRequestHeader('Accept', 'application/json');
+    }
+}
