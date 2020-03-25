1e38a7e9df566c9c98377f2c7d1661b25f823f18
Carles Pina i Estany <carles@pina.cat>
2020-03-25 16:13:14 +0100

    Renames Javascript variabl and functions to follow using Javascript conventions instead of Python

--- ProjectApplication/project_core/static/js/orcid.js
+++ ProjectApplication/project_core/static/js/orcid.js
@@ -1,79 +1,67 @@
-function setup_orcid_lookup(form_prefix, orcid_field, first_name_field, surname_field) {
-    let button_selector = "#div_id_" + form_prefix + "-" + orcid_field + " .input-group-append";
-    let icon_selector = button_selector + " .input-group-text i";
+function setupOrcidLookup(formPrefix, orcidFieldName, firstNameFieldName, surnameFieldName) {
+    let buttonSelector = "#div_id_" + formPrefix + "-" + orcidFieldName + " .input-group-append";
+    let iconSelector = buttonSelector + " .input-group-text i";
 
-    let orcid_field_selector = '#id_' + form_prefix + '-' + orcid_field;
-    let first_name_selector = '#id_' + form_prefix + '-' + first_name_field;
-    let surname_selector = '#id_' + form_prefix + '-' + surname_field;
+    let orcidFieldSelector = '#id_' + formPrefix + '-' + orcidFieldName;
+    let firstNameSelector = '#id_' + formPrefix + '-' + firstNameFieldName;
+    let surnameSelector = '#id_' + formPrefix + '-' + surnameFieldName;
 
-    let typing_timer;
+    let typingTimer;
 
-    const initial_icon_class = $(icon_selector).attr('class');  // It assumes that all the ORCID iD icons are the same,
+    const INITIAL_ICON_CLASS = $(iconSelector).attr('class');  // It assumes that all the ORCID iD icons are the same,
                                                                 // (this function is used for different of them and stores
                                                                 // the first attr)
-    const initial_icon_style = $(icon_selector).attr('style');
+    const INITIAL_ICON_STYLE = $(iconSelector).attr('style');
 
-    let start_lookup = function () {
-        function success(orcid_record) {
-            let family_name = orcid_record['person']['name']['family-name'].value;
-            let given_names = orcid_record['person']['name']['given-names'].value;
+    let startLookup = function () {
+        function success(orcidRecord) {
+            let familyName = orcidRecord['person']['name']['family-name'].value;
+            let givenNames = orcidRecord['person']['name']['given-names'].value;
 
-            $(first_name_selector).val(given_names);
-            $(surname_selector).val(family_name);
+            $(firstNameSelector).val(givenNames);
+            $(surnameSelector).val(familyName);
 
-            $(icon_selector).attr('class', 'fas fa-check');
-            $(icon_selector).attr('style', initial_icon_style);
+            $(iconSelector).attr('class', 'fas fa-check');
+            $(iconSelector).attr('style', INITIAL_ICON_STYLE);
         }
 
         function error() {
-            $(icon_selector).attr('class', 'fas fa-exclamation');
-            $(icon_selector).attr('style', 'color:#ff0000');
+            $(iconSelector).attr('class', 'fas fa-exclamation');
+            $(iconSelector).attr('style', 'color:#ff0000');
         }
 
-        // let orcid_id = $('#id_' + form_prefix + '-' + orcid_field + '.textinput.textInput.form-control').val();
-        let orcid_id = $(orcid_field_selector).val();
-        getOrcid(orcid_id, success, error);
-        $(icon_selector).attr('class', 'fas fa-spinner fa-spin');
+        // let orcid_id = $('#id_' + formPrefix + '-' + orcidFieldName + '.textinput.textInput.form-control').val();
+        let orcidId = $(orcidFieldSelector).val();
+        getOrcid(orcidId, success, error);
+        $(iconSelector).attr('class', 'fas fa-spinner fa-spin');
     };
 
-    $(button_selector).click(start_lookup);
+    $(buttonSelector).click(startLookup);
 
-    $(orcid_field_selector).on('input', function () {
-        let done_typing = function () {
-            start_lookup();
+    $(orcidFieldSelector).on('input', function () {
+        let doneTyping = function () {
+            startLookup();
         };
-        clearTimeout(typing_timer);
+        clearTimeout(typingTimer);
 
-        $(first_name_selector).val('');
-        $(surname_selector).val('');
-        $(icon_selector).attr('class', initial_icon_class);
-        $(icon_selector).attr('style', initial_icon_style);
+        $(firstNameSelector).val('');
+        $(surnameSelector).val('');
+        $(iconSelector).attr('class', INITIAL_ICON_CLASS);
+        $(iconSelector).attr('style', INITIAL_ICON_STYLE);
 
-        typing_timer = setTimeout(done_typing, 100);
+        typingTimer = setTimeout(doneTyping, 100);
     });
 }
 
-function getOrcid(orcid_id, success_function, error_function) {
+function getOrcid(orcidId, successFunction, errorFunction) {
     $.ajax({
-        url: "https://pub.orcid.org/v3.0/" + orcid_id,
+        url: "https://pub.orcid.org/v3.0/" + orcidId,
         type: "GET",
         dataType: "JSON",
         beforeSend: setHeader,
-        success: success_function,
-        error: error_function
+        success: successFunction,
+        error: errorFunction
     });
-
-    function orcid_success(orcid_record) {
-        // For debugging
-        let family_name = orcid_record['person']['name']['family-name'].value;
-        let given_names = orcid_record['person']['name']['given-names'].value;
-        alert('orcid success: ' + given_names + " " + family_name);
-    }
-
-    function orcid_error() {
-        // For debugging
-        alert('orcid failure');
-    }
 
     function setHeader(request) {
         request.setRequestHeader('Accept', 'application/json');
