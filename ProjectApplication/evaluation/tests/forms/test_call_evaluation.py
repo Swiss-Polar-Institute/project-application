0de5f1b30b0ea3fe4b4a1b8eb71331f57c5f21e2
Carles Pina i Estany <carles@pina.cat>
2020-03-27 12:39:32 +0100

    Adds unit test for CallEvaluation

---
+++ ProjectApplication/evaluation/tests/forms/test_call_evaluation.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from django.test import TestCase
+
+from evaluation.forms.call_evaluation import CallEvaluationForm
+from evaluation.models import CallEvaluation
+from project_core.tests import database_population
+
+
+class CallEvaluationFormTest(TestCase):
+    def setUp(self):
+        self._proposal = database_population.create_proposal()
+        self._reviewer_user = database_population.create_reviewer_user()
+        self._management_user = database_population.create_management_user()
+
+    def test_save_call_evaluation(self):
+        data = {'call': self._proposal.call,
+                'panel_date': datetime.today(),
+                }
+
+        self.assertEqual(CallEvaluation.objects.all().count(), 0)
+
+        call_evaluation_form = CallEvaluationForm(data=data, call=self._proposal.call)
+        self.assertTrue(call_evaluation_form.is_valid())
+
+        self.assertRaises(PermissionError, call_evaluation_form.save_call_evaluation, self._reviewer_user)
+
+        call_evaluation_form.save_call_evaluation(self._management_user)
+
+        self.assertEqual(CallEvaluation.objects.all().count(), 1)
