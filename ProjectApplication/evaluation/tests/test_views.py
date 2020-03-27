b2b9e10b27b22beeca92efb702223d195a9b05cc
Carles Pina i Estany <carles@pina.cat>
2020-03-27 13:49:38 +0100

    Adds unit tests for CallEvaluation / ProposalEvaluation
    Still more to do and the "post"

--- ProjectApplication/evaluation/tests/test_views.py
+++ ProjectApplication/evaluation/tests/test_views.py
@@ -1,7 +1,10 @@
-from django.test import TestCase
+from datetime import datetime
+
+from django.test import TestCase, Client
+from django.urls import reverse
 
 from ProjectApplication import settings
-from evaluation.models import ProposalEvaluation
+from evaluation.models import ProposalEvaluation, CallEvaluation
 from evaluation.views import CallEvaluationValidation
 from project_core.models import Proposal, ProposalStatus
 from project_core.tests import database_population
@@ -58,3 +61,67 @@ class CallEvaluationSummaryViewTest(TestCase):
         # Now it has the proposal evaluation, it's all good
         check_result = CallEvaluationValidation._check_all_submitted_proposals_have_eligibility_set(proposals)
         self.assertEqual(check_result['proposals'].count(), 0)
+
+
+class ProposalEvaluationDetailTest(TestCase):
+    def setUp(self):
+        self._proposal = database_population.create_proposal()
+        self._user = database_population.create_management_user()
+
+    def test_proposal_evaluation_detail(self):
+        c = Client()
+        c.login(username='unittest_management', password='12345')
+        response = c.get(reverse('logged-proposal-evaluation-add') + f'?proposal={self._proposal.id}')
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_proposal_evaluation_detail_permission_denied(self):
+        c = Client()
+        response = c.get(reverse('logged-proposal-evaluation-add') + f'?proposal={self._proposal.id}')
+
+        self.assertEqual(response.status_code, 302)
+
+
+class ProposalEvaluationListTest(TestCase):
+    def setUp(self):
+        self._user = database_population.create_management_user()
+
+    def test_list(self):
+        c = Client()
+        c.login(username='unittest_management', password='12345')
+        response = c.get(reverse('logged-evaluation-list'))
+
+        self.assertEqual(response.status_code, 200)
+
+
+class CallEvaluationUpdateTest(TestCase):
+    def setUp(self):
+        self._user = database_population.create_management_user()
+        self._proposal = database_population.create_proposal()
+
+    def test_detail(self):
+        call_evaluation = CallEvaluation()
+        call_evaluation.call = self._proposal.call
+        call_evaluation.panel_date = datetime.today()
+        call_evaluation.save()
+
+        c = Client()
+        c.login(username='unittest_management', password='12345')
+        response = c.get(reverse('logged-call-evaluation-update', kwargs={'pk': call_evaluation.id}))
+
+        self.assertEqual(response.status_code, 200)
+
+
+class ProposalListTest(TestCase):
+    def setUp(self):
+        self._user = database_population.create_management_user()
+        self._proposal = database_population.create_proposal()
+
+    def test_detail(self):
+        c = Client()
+        c.login(username='unittest_management', password='12345')
+        response = c.get(reverse('logged-call-evaluation-list-proposals', kwargs={'call_id': self._proposal.call.id}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context_data['call'], self._proposal.call)
+        self.assertEqual(response.context_data['proposals'].count(), 0)
