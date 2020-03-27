f02dea76e072d8abc6983201830de9f198af8036
Carles Pina i Estany <carles@pina.cat>
2020-03-27 12:53:21 +0100

    Fixes unit test - incorrect name made it not run before.

--- ProjectApplication/evaluation/tests/test_views.py
+++ ProjectApplication/evaluation/tests/test_views.py
@@ -33,24 +33,28 @@ class CallEvaluationSummaryViewTest(TestCase):
         check_result = CallEvaluationValidation._check_all_submitted_proposals_have_eligibility_set(proposals)
         self.assertEqual(check_result['proposals'].count(), 0)
 
-    def check_eligible_proposals_have_evaluation(self):
+    def test_check_eligible_proposals_have_evaluation(self):
         proposal = database_population.create_proposal()
 
         proposal.eligibility = Proposal.ELIGIBILITYNOTCHECKED
         proposal.proposal_status = ProposalStatus.objects.get(name=settings.PROPOSAL_STATUS_SUBMITTED)
-        proposal.eligibility = Proposal.ELIGIBLE
         proposal.save()
 
         proposals = Proposal.objects.filter(uuid=proposal.uuid)
 
         # Expects one proposal that is eligible but doesn't have a proposal evaluation
-        check_result = CallEvaluationValidation._check_eligible_proposals_have_evaluation(proposals)
+        check_result = CallEvaluationValidation._check_all_submitted_proposals_have_eligibility_set(proposals)
         self.assertEqual(check_result['proposals'].count(), 1)
 
         proposal_evaluation = ProposalEvaluation()
         proposal_evaluation.proposal = proposal
         proposal_evaluation.save()
 
+        proposal.eligibility = Proposal.ELIGIBLE
+        proposal.save()
+
+        proposals = Proposal.objects.filter(uuid=proposal.uuid)
+
         # Now it has the proposal evaluation, it's all good
-        check_result = CallEvaluationValidation._check_eligible_proposals_have_evaluation(proposals)
+        check_result = CallEvaluationValidation._check_all_submitted_proposals_have_eligibility_set(proposals)
         self.assertEqual(check_result['proposals'].count(), 0)
