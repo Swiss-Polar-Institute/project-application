636cbb6b0b445981108d2dc5248579750b673a6b
dalgetty <john.maxwell@epfl.ch>
2020-04-03 12:50:39 +0200

    First attempt at Admin interface

--- ProjectApplication/grant_management/admin.py
+++ ProjectApplication/grant_management/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import (GrantAgreement)
+
 # Register your models here.
+admin.site.register(GrantAgreement)
+
+class GrantAdmin(admin.ModelAdmin):
+    list_display = ('project','signed_date','signed_by','file')
+    fields = ['project', ('signed_date', 'signed_by'),'file']
