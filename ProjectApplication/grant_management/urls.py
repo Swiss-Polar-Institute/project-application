ba90218b3ccb309e409ad7d5719fb816a7684567
Carles Pina i Estany <carles@pina.cat>
2020-04-06 12:15:47 +0200

    WIP - Reformats existing Detail template (Template Question and Funding Instrument) to have the same format as other Detail views
    Adds project list able in the grant_management and basic format for the
    project-detail

--- ProjectApplication/grant_management/urls.py
+++ ProjectApplication/grant_management/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 import grant_management.views
 
 urlpatterns = [
-    path('logged/grant-management/project_list/',
+    path('logged/grant-management/project/list/',
          grant_management.views.ProjectList.as_view(),
          name='logged-grant_management-project-list'),
+    path('logged/grant-management/project/<int:pk>/',
+         grant_management.views.ProjectDetail.as_view(),
+         name='logged-grant_management-project-detail'),
 ]
--- ProjectApplication/grant_management/views.py
+++ ProjectApplication/grant_management/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import TemplateView
+from django.urls import reverse
+from django.views.generic import TemplateView, DetailView
 
 from project_core.models import Project
 
@@ -20,3 +21,23 @@ class ProjectList(TemplateView):
         context['breadcrumb'] = [{'name': 'Grant management'}]
 
         return context
+
+
+class ProjectDetail(DetailView):
+    template_name = 'grant_management/project-detail.tmpl'
+    context_object_name = 'project'
+    model = Project
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context.update({'active_section': 'grant_management',
+                        'active_subsection': 'project-list',
+                        'sidebar_template': 'grant_management/_sidebar-grant_management.tmpl'})
+
+        context['breadcrumb'] = [{'name': 'Grant management', 'url': reverse('logged-grant_management-project-list')},
+                                 {'name': 'Project detail'}]
+
+        return context
+
+
