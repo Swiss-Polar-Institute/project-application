c108564a897d9338756f37d3c69c8b15b5b1967a
Carles Pina i Estany <carles@pina.cat>
2020-04-07 10:51:29 +0200

    PEP-8

--- ProjectApplication/grant_management/admin.py
+++ ProjectApplication/grant_management/admin.py
@@ -1,50 +1,52 @@
 from django.contrib import admin
 
-from .models import (GrantAgreement, Invoice, FinanceReport, LaySummary, License, Media, ProjectSocialMedia, Publication, Dataset)
+from .models import (GrantAgreement, Invoice, FinanceReport, LaySummary, License, Media, ProjectSocialMedia,
+                     Publication, Dataset)
+
 
 class GrantAdmin(admin.ModelAdmin):
-    list_display = ('project','signed_date','signed_by','file')
-    fields = ['project', 'signed_date', 'signed_by','file']
+    list_display = ('project', 'signed_date', 'signed_by', 'file')
+    fields = ['project', 'signed_date', 'signed_by', 'file']
 
 
 class InvoiceAdmin(admin.ModelAdmin):
-    list_display = ('project','amount','sent_date','reception_date','due_date','paid_date','urls','file')
-    fields = ['project','amount','sent_date','reception_date','due_date','paid_date','urls','file']
+    list_display = ('project', 'amount', 'sent_date', 'reception_date', 'due_date', 'paid_date', 'urls', 'file')
+    fields = ['project', 'amount', 'sent_date', 'reception_date', 'due_date', 'paid_date', 'urls', 'file']
 
 
 class FinanceReportAdmin(admin.ModelAdmin):
-    list_display = ('sent_for_approval_date','signed_by','file')
-    fields = ['sent_for_approval_date','signed_by','file']
+    list_display = ('sent_for_approval_date', 'signed_by', 'file')
+    fields = ['sent_for_approval_date', 'signed_by', 'file']
 
 
 class LaySummaryAdmin(admin.ModelAdmin):
-    list_display = ('project','text','author','web_version','due_date','sent_date','reception_date')
-    fields = ['project','text','author','web_version','due_date','sent_date','reception_date']
+    list_display = ('project', 'text', 'author', 'web_version', 'due_date', 'sent_date', 'reception_date')
+    fields = ['project', 'text', 'author', 'web_version', 'due_date', 'sent_date', 'reception_date']
 
 
 class LicenseAdmin(admin.ModelAdmin):
-    list_display = ('name','public_text')
-    fields = ['name','public_text']
+    list_display = ('name', 'public_text')
+    fields = ['name', 'public_text']
 
 
 class MediaAdmin(admin.ModelAdmin):
-    list_display = ('author','license','copyright','project','due_date','sent_date','reception_date')
-    fields = ['author','license','copyright','project','due_date','sent_date','reception_date']
+    list_display = ('author', 'license', 'copyright', 'project', 'due_date', 'sent_date', 'reception_date')
+    fields = ['author', 'license', 'copyright', 'project', 'due_date', 'sent_date', 'reception_date']
 
 
 class ProjectSocialMediaAdmin(admin.ModelAdmin):
-    list_display = ('project','social_network','url')
-    fields = ['project','social_network','url']
+    list_display = ('project', 'social_network', 'url')
+    fields = ['project', 'social_network', 'url']
 
 
 class PublicationAdmin(admin.ModelAdmin):
-    list_display = ('project','doi','reference','title','date_published')
-    fields = ['project','doi','reference','title','date_published']
+    list_display = ('project', 'doi', 'reference', 'title', 'date_published')
+    fields = ['project', 'doi', 'reference', 'title', 'date_published']
 
 
 class DatasetAdmin(admin.ModelAdmin):
-    list_display = ('project','doi','url','title','date_published')
-    fields = ['project','doi','url','title','date_published']
+    list_display = ('project', 'doi', 'url', 'title', 'date_published')
+    fields = ['project', 'doi', 'url', 'title', 'date_published']
 
 
 # Register your models here.
@@ -57,4 +59,3 @@ admin.site.register(Media)
 admin.site.register(ProjectSocialMedia)
 admin.site.register(Publication)
 admin.site.register(Dataset)
-
