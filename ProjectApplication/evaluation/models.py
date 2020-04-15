c75e1394345a27f87e4ec1de87e12f03ea144d84
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-04-15 18:49:12 +0200

    Added help text to ProposalEvaluation fields in models.

--- ProjectApplication/evaluation/models.py
+++ ProjectApplication/evaluation/models.py
@@ -72,15 +72,17 @@ class ProposalEvaluation(CreateModifyOn):
     allocated_budget = models.DecimalField(help_text='Allocated budget', decimal_places=2, max_digits=10,
                                            validators=[MinValueValidator(0)], blank=True, null=True)
 
-    panel_remarks = models.TextField(blank=True, null=True)
-    feedback_to_applicant = models.TextField(blank=True, null=True)
-    panel_recommendation = models.CharField(choices=PANEL_RECOMMENDATION, max_length=7, blank=True, null=True)
-    board_decision = models.CharField(choices=BOARD_DECISION, max_length=7, blank=True, null=True)
-    decision_date = models.DateField(blank=True, null=True)
+    panel_remarks = models.TextField(help_text='Remarks made by the panel regarding the proposal', blank=True, null=True)
+    feedback_to_applicant = models.TextField(help_text="Details of the panel's feedback to the applicant", blank=True, null=True)
+    panel_recommendation = models.CharField(choices=PANEL_RECOMMENDATION, max_length=7,
+                                            help_text='Recommendation made by the panel', blank=True, null=True)
+    board_decision = models.CharField(choices=BOARD_DECISION, max_length=7, help_text='Decision made by the board', blank=True, null=True)
+    decision_date = models.DateField(help_text="Date on which the board's decision was made", blank=True, null=True)
     decision_letter = models.FileField(storage=storages.backends.s3boto3.S3Boto3Storage(),
                                        upload_to=proposal_evaluation_eligibility_letter_rename,
+                                       help_text='Decision letter file sent to applicant',
                                        blank=True, null=True)
-    decision_letter_date = models.DateField(blank=True, null=True)
+    decision_letter_date = models.DateField(help_text='Date on which the decision letter was sent', blank=True, null=True)
     history = HistoricalRecords()
 
     def __str__(self):
