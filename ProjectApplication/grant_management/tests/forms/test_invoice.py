335abd748e7b3fd15a8cc32c18322b4cb849e2ff
Carles Pina i Estany <carles@pina.cat>
2020-04-21 18:20:42 +0200

    Uploads a full invoice for the unit test

--- ProjectApplication/grant_management/tests/forms/test_invoice.py
+++ ProjectApplication/grant_management/tests/forms/test_invoice.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-from io import StringIO
 
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 
 from grant_management.forms.invoices import InvoiceItemModelForm
+from grant_management.models import GrantAgreement
 from project_core.tests import database_population
 
 
@@ -22,9 +22,19 @@ class InvoiceItemFormTest(TestCase):
         self.assertTrue(invoice_item_form.is_valid())
 
     def test_valid_complete_invoice(self):
+        grant_agreement = GrantAgreement(project=self._project,
+                                         signed_date=datetime(2020, 1, 4),
+                                         signed_by=database_population.create_physical_person(),
+                                         file=SimpleUploadedFile('grant_agreement.txt',
+                                                                 b'This is the signed grant agreement. C.'))
+        grant_agreement.save()
+
         data = {'project': self._project,
                 'due_date': datetime(2020, 1, 5),
                 'reception_date': datetime(2020, 1, 6),
+                'sent_date': datetime(2020, 1, 7),
+                'amount': 200,
+                'paid_date': datetime(2020, 1, 8)
                 }
         file = {'file': SimpleUploadedFile('file.txt', b'some file content')}
 
