68703b31e2db9111248fc0498eef0c8f27ee77e1
Carles Pina i Estany <carles@pina.cat>
2020-04-22 18:48:31 +0200

    Adds unit test for a valid invoice

--- ProjectApplication/grant_management/tests/test_views.py
+++ ProjectApplication/grant_management/tests/test_views.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.utils.datastructures import MultiValueDict
 
+from grant_management.models import Invoice, FinancialReport
 from project_core.tests import database_population
 
 
@@ -10,7 +12,7 @@ class ProjectListTest(TestCase):
         self._client_management = database_population.create_management_logged_client()
 
     def test_get(self):
-        response = self._client_management.get(reverse('logged-grant_management-project-list'))
+        response = self._client_management.get(reverse('logged-grant_management-project-lis'))
         self.assertEqual(response.status_code, 200)
 
         self.assertContains(response, self._project.title)
@@ -40,3 +42,28 @@ class FinancesViewUpdateTest(TestCase):
             reverse('logged-grant_management-finances-update', kwargs={'project': self._project.id})
         )
         self.assertEqual(response.status_code, 200)
+
+    def test_post_valid_invoice(self):
+        project_id = self._project.id
+        data = MultiValueDict(
+            {'invoices_form-TOTAL_FORMS': ['1'], 'invoices_form-INITIAL_FORMS': ['0'],
+             'invoices_form-MIN_NUM_FORMS': ['1'], 'invoices_form-MAX_NUM_FORMS': ['1000'],
+             'invoices_form-0-project': [str(project_id)], 'invoices_form-0-id': [''], 'invoices_form-0-DELETE': [''],
+             'invoices_form-0-can_be_deleted': ['0'], 'invoices_form-0-due_date': ['22-01-2021'],
+             'financial_reports_form-TOTAL_FORMS': ['1'], 'financial_reports_form-INITIAL_FORMS': ['1'],
+             'financial_reports_form-MIN_NUM_FORMS': ['1'], 'financial_reports_form-MAX_NUM_FORMS': ['1000'],
+             'financial_reports_form-0-project': [str(project_id)], 'financial_reports_form-0-id': [''],
+             'financial_reports_form-0-DELETE': [''], 'financial_reports_form-0-can_be_deleted': ['0'],
+             'financial_reports_form-0-file': [''], 'save_finances': ['Save Finances']
+             })
+
+        self.assertEqual(Invoice.objects.all().count(), 0)
+        self.assertEqual(FinancialReport.objects.all().count(), 0)
+
+        response = self._client_management.post(
+            reverse('logged-grant_management-finances-update', kwargs={'project': self._project.id}),
+            data=data
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Invoice.objects.all().count(), 1)
