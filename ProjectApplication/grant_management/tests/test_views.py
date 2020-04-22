a49c3467ebd1361ef688a3ae2f51b2760268194a
Carles Pina i Estany <carles@pina.cat>
2020-04-22 15:51:25 +0200

    Basic tests for the grant_management views

---
+++ ProjectApplication/grant_management/tests/test_views.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from project_core.tests import database_population
+
+
+class ProjectListTest(TestCase):
+    def setUp(self):
+        self._project = database_population.create_project()
+        self._client_management = database_population.create_management_logged_client()
+
+    def test_get(self):
+        response = self._client_management.get(reverse('logged-project-list'))
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, self._project.title)
+
+
+class ProjectDetailsTest(TestCase):
+    def setUp(self):
+        self._project = database_population.create_project()
+        self._client_management = database_population.create_management_logged_client()
+
+    def test_get(self):
+        response = self._client_management.get(
+            reverse('logged-grant_management-project-detail', kwargs={'pk': self._project.id}))
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, self._project.title)
+
+
+class FinancesViewUpdateTest(TestCase):
+    def setUp(self):
+        self._project = database_population.create_project()
+        self._client_management = database_population.create_management_logged_client()
+
+    def test_get(self):
+        response = self._client_management.get(
+            reverse('logged-grant_management-finances-update', kwargs={'project': self._project.id})
+        )
+        self.assertEqual(response.status_code, 200)
