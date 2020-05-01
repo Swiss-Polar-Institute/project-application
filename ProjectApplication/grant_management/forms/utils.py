21320b81e26b7681f4405bcb4aab34273eb76763
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-05-01 13:49:39 +0200

    Amended error text in finance form.

--- ProjectApplication/grant_management/forms/utils.py
+++ ProjectApplication/grant_management/forms/utils.py
@@ -2,11 +2,11 @@ from project_core.utils.utils import format_date
 
 
 def error_due_date_too_early(project_start_date):
-    return f'Due date should to be after project start date ({format_date(project_start_date)})'
+    return f'Due date should be after project start date ({format_date(project_start_date)})'
 
 
 def error_due_date_too_late(project_end_date):
-    return f'Due date should to be before the project ends date ({format_date(project_end_date)})'
+    return f'Due date should be before the project ends date ({format_date(project_end_date)})'
 
 
 def error_received_date_too_early(project_start_date):
