4bade05380e351433df159e64fbecf543adac1b6
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-05-01 14:14:53 +0200

    Amended finance report due date error text.

--- ProjectApplication/grant_management/forms/utils.py
+++ ProjectApplication/grant_management/forms/utils.py
@@ -6,7 +6,7 @@ def error_due_date_too_early(project_start_date):
 
 
 def error_due_date_too_late(project_end_date):
-    return f'Due date should be before the project ends date ({format_date(project_end_date)})'
+    return f'Due date should be before the project end date ({format_date(project_end_date)})'
 
 
 def error_received_date_too_early(project_start_date):
