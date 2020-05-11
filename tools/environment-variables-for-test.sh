ea1f9bc588fdbdb99467c2e35733be243d18f691
Carles Pina i Estany <carles@pina.cat>
2020-05-11 09:26:38 +0200

    Avoids skipping unit tests
    Uses minio on Github

--- tools/environment-variables-for-test.sh
+++ tools/environment-variables-for-test.sh
@@ -1,7 +1,7 @@
-export SKIP_S3_TESTS=1
+# export SKIP_S3_TESTS=1
 export FORCE_SQLITE3_DATABASE=1
 
-export OBJECT_STORAGE_SECRET_ACCESS_KEY=not_needed_for_tests
+export OBJECT_STORAGE_SECRET_ACCESS_KEY=this_is_used_for_the_unit_tests
 export OBJECT_STORAGE_BUCKET_NAME=projects
 export OBJECT_STORAGE_PREFIX_LOCATION=projects
 export FROM_EMAIL=notification@example.com
@@ -12,5 +12,5 @@ export EMAIL_SUBJECT_PREFIX=[ProjectsApplicationUnitTest]
 export ADMIN_1=Name,name@example.com
 export DEBUG=1
 export ALLOWED_HOST_1=127.0.0.1
-export OBJECT_STORAGE_ACCESS_KEY_ID=key
+export OBJECT_STORAGE_ACCESS_KEY_ID=used_for_the_unit_tests
 export OBJECT_STORAGE_ENDPOINT_URL=http://localhost:9000
