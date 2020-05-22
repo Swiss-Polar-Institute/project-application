5fe748c0b79218f7d9797108d7a4687f3398e735
Carles Pina i Estany <carles@pina.cat>
2020-05-22 16:35:38 +0200

    File names with an icon, ellided and tooltip

--- ProjectApplication/project_core/templatetags/filename_from_path.py
+++ ProjectApplication/project_core/templatetags/filename_from_path.py
@@ -8,5 +8,22 @@ register = template.Library()
 
 @register.filter
 @stringfilter
+def short_filename_from_path(path):
+    filename = os.path.basename(path)
+    max_length = 12
+
+    base_filename, extension = os.path.splitext(filename)
+
+    if len(base_filename) > max_length - 3:
+        if extension.startswith('.'):
+            extension = extension[1:]
+
+        filename = base_filename[:max_length] + '…' + extension
+
+    return filename
+
+
+@register.filter
+@stringfilter
 def filename_from_path(path):
     return os.path.basename(path)
