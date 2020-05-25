35f1d2ed89a32d0127ab54cb30d2a09d07648990
Carles Pina i Estany <carles@pina.cat>
2020-05-25 16:54:32 +0200

    Adds unit test for ordinal

---
+++ ProjectApplication/project_core/tests/templatetags/test_ordinal.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+from ...templatetags.ordinal import ordinal
+
+
+class OrdinalTest(TestCase):
+    def setUp(self):
+        pass
+
+    def ordinal_test(self):
+        self.assertEqual(ordinal(1), 'First')
+        self.assertEqual(ordinal(2), 'Second')
+        self.assertEqual(ordinal(11), '11th')
+        self.assertEqual(ordinal(None), None)
