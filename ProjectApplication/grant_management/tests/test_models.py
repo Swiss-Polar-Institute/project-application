54233f55b3b46c28744d9318f002206c1b34ca79
Carles Pina i Estany <carles@pina.cat>
2020-05-25 16:48:56 +0200

    Adds unit test for the Invoice model

---
+++ ProjectApplication/grant_management/tests/test_models.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+
+from project_core.tests import database_population
+from ..models import Invoice, Installment
+
+
+class InvoiceTest(TestCase):
+    def setUp(self):
+        self._project = database_population.create_project()
+
+    def installment_number_test(self):
+        installment = Installment.objects.create(project=self._project,
+                                                 due_date=self._project.end_date,
+                                                 amount=200)
+
+        invoice = Invoice.objects.create(project=self._project,
+                                         installment=installment,
+                                         due_date=self._project.end_date,
+                                         amount=200)
+
+        self.assertEqual(invoice.installment_number(), 1)
+
+    def installment_number_none_test(self):
+        invoice = Invoice.objects.create(project=self._project,
+                                         installment=None,
+                                         due_date=self._project.end_date,
+                                         amount=200)
+
+        self.assertIsNone(invoice.installment_number())
