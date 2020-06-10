9944c962042ba8f212dd62241edc0548ec818662
Carles Pina i Estany <carles@pina.cat>
2020-06-10 12:50:59 +0200

    Add unit test for closing a completed project

--- ProjectApplication/grant_management/tests/forms/test_close_project.py
+++ ProjectApplication/grant_management/tests/forms/test_close_project.py
@@ -9,7 +9,7 @@ class CloseProjectFormTest(TestCase):
     def setUp(self):
         self._project = database_population.create_project()
 
-    def test_medium_valid(self):
+    def test_aborted(self):
         data = {'status': 'Aborted',
                 'abortion_reason': 'Permit did not arrive on time',
                 'close': 'Close'
@@ -24,3 +24,19 @@ class CloseProjectFormTest(TestCase):
         close_project_form.save()
 
         self.assertEqual(self._project.status, Project.ABORTED)
+
+    def test_completed(self):
+        data = {'status': 'Completed',
+                'abortion_reason': '',
+                'close': 'Close'
+                }
+
+        self.assertEqual(self._project.status, Project.ONGOING)
+
+        close_project_form = CloseProjectModelForm(data=data, instance=self._project)
+
+        self.assertTrue(close_project_form.is_valid())
+
+        close_project_form.save()
+
+        self.assertEqual(self._project.status, Project.COMPLETED)
