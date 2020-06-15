bdf0cec622d40c29827269f31ed71bc326d455d0
Carles Pina i Estany <carles@pina.cat>
2020-06-15 15:32:21 +0200

    Improve help text: add link to edit the project basic information

--- ProjectApplication/grant_management/forms/utils.py
+++ ProjectApplication/grant_management/forms/utils.py
@@ -1,13 +1,28 @@
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
 from project_core.utils.utils import format_date
 
 
 def error_due_date_too_early(project):
-    return f'Due date should be after project start date ({format_date(project.start_date)})'
+    project_basic_information_edit = reverse('logged-grant_management-project-basic-information-update',
+                                             kwargs={'project': project.id})
+
+    return mark_safe(f'Due date should be after project start date ({format_date(project.start_date)})'
+                     f'If needed <a href="{project_basic_information_edit}">edit the project start date</a>.')
 
 
 def error_due_date_too_late(project):
-    return f'Due date should be before the project end date ({format_date(project.end_date)})'
+    project_basic_information_edit = reverse('logged-grant_management-project-basic-information-update',
+                                             kwargs={'project': project.id})
+    return mark_safe(
+        f'Due date should be before the project end date ({format_date(project.end_date)}). '
+        f'If needed <a href="{project_basic_information_edit}">edit the project end date</a>.')
 
 
 def error_received_date_too_early(project):
-    return f'Date received should be after project start date ({format_date(project.start_date)})'
+    project_basic_information_edit = reverse('logged-grant_management-project-basic-information-update',
+                                             kwargs={'project': project.id})
+
+    return mark_safe(f'Date received should be after project start date ({format_date(project.start_date)})'
+                     f'If needed <a href="{project_basic_information_edit}">edit the project start date</a>.')
