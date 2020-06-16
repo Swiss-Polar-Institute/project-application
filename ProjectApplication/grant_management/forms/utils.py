6195feff0417afcf900541ab4fa44b2a39473fa4
Carles Pina i Estany <carles@pina.cat>
2020-06-16 16:54:30 +0200

    Fix string (missing full dot/space)

--- ProjectApplication/grant_management/forms/utils.py
+++ ProjectApplication/grant_management/forms/utils.py
@@ -8,7 +8,7 @@ def error_due_date_too_early(project):
     project_basic_information_edit = reverse('logged-grant_management-project-basic-information-update',
                                              kwargs={'project': project.id})
 
-    return mark_safe(f'Due date should be after project start date ({format_date(project.start_date)})'
+    return mark_safe(f'Due date should be after project start date ({format_date(project.start_date)}). '
                      f'If needed <a href="{project_basic_information_edit}">edit the project start date</a>.')
 
 
@@ -24,5 +24,5 @@ def error_received_date_too_early(project):
     project_basic_information_edit = reverse('logged-grant_management-project-basic-information-update',
                                              kwargs={'project': project.id})
 
-    return mark_safe(f'Date received should be after project start date ({format_date(project.start_date)})'
+    return mark_safe(f'Date received should be after project start date ({format_date(project.start_date)}). '
                      f'If needed <a href="{project_basic_information_edit}">edit the project start date</a>.')
