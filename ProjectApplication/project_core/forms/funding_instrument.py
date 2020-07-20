8271282c43d991e9142907fefb8cd2fd8839f282
Carles Pina i Estany <carles@pina.cat>
2020-07-20 18:31:38 +0200

    Shows the current financial key when editing the funding instruments

--- ProjectApplication/project_core/forms/funding_instrument.py
+++ ProjectApplication/project_core/forms/funding_instrument.py
@@ -1,5 +1,6 @@
 from crispy_forms.helper import FormHelper
 from django import forms
+from django.db.models import Q
 from django.urls import reverse
 
 from ..models import FundingInstrument, FinancialKey
@@ -13,7 +14,8 @@ class FundingInstrumentForm(forms.ModelForm):
         self.helper.form_tag = False
 
         # funding_instrument is the Boolean field. fundinginstrument is the reverse relationship for the OneToOne
-        self.fields['short_name'].queryset = FinancialKey.objects.filter(funding_instrument=True).filter(fundinginstrument__isnull=True)
+        self.fields['short_name'].queryset = FinancialKey.objects.filter(funding_instrument=True).filter(
+            Q(fundinginstrument__isnull=True) | Q(fundinginstrument=self.instance))
 
         self.fields[
             'short_name'].help_text = f'Select a funding instrument acronym. This needs to be a financial key. Please <a href="{reverse("logged-financial-key-list")}">create one if needed</a> and refresh this page'
