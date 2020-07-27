40be973c6828088883581c64aa64a0ddffe04c91
Carles Pina i Estany <carles@pina.cat>
2020-07-27 14:44:03 +0200

    Add more reporting

--- ProjectApplication/reporting/views.py
+++ ProjectApplication/reporting/views.py
@@ -1,23 +1,40 @@
 from django.shortcuts import render
 from django.utils import timezone
 from django.views.generic import TemplateView
-from django.db.models import Avg, Max, Min, Count
+from django.db.models import Avg, Max, Min, Count, F, Sum
 
-from project_core.models import Call
+from project_core.models import Call, Project
 
 
 def calculate_number_of_calls():
     result = {}
-    minimum_year = Call.objects.filter(submission_deadline__lte=timezone.now()).aggregate(Min('finance_year'))[
-        'finance_year__min']
-    maximum_year = Call.objects.filter(submission_deadline__lte=timezone.now()).aggregate(Max('finance_year'))[
-        'finance_year__max']
 
-    result['minimum_year'] = minimum_year
-    result['maximum_year'] = maximum_year
+    result['calls_per_year'] = Call.objects.filter(submission_deadline__lte=timezone.now()). \
+        values(year=F('finance_year')). \
+        annotate(aggregated=Count('*')). \
+        order_by('finance_year')
 
-    result['calls_per_year'] = Call.objects.filter(submission_deadline__lte=timezone.now()).values(
-        'finance_year').annotate(number_of_calls=Count('id')).order_by('finance_year')
+    return result
+
+
+def allocated_budget_per_year():
+    result = {}
+
+    result['allocated_budget_per_year'] = Project.objects. \
+        values(year=F('call__finance_year')). \
+        annotate(aggregated=Sum('allocated_budget')). \
+        order_by('call__finance_year')
+
+    return result
+
+
+def allocated_budget_per_call():
+    result = {}
+
+    result['allocated_budget_per_call'] = Project.objects. \
+        values(year=F('call__long_name')). \
+        annotate(aggregated=Sum('allocated_budget')). \
+        order_by('call__finance_year')
 
     return result
 
@@ -29,6 +46,10 @@ class Reporting(TemplateView):
         context = super().get_context_data(**kwargs)
 
         context.update(calculate_number_of_calls())
+
+        context.update(allocated_budget_per_year())
+
+        context.update(allocated_budget_per_call())
 
         context.update({'active_section': 'reporting',
                         'active_subsection': 'reporting',
