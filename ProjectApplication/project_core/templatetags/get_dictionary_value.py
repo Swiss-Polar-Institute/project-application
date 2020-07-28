06cf2b6c4b81558bc433864388d9b49aa5e48ad4
Carles Pina i Estany <carles@pina.cat>
2020-07-28 17:15:37 +0200

    Stop printing (used for debugging)

--- ProjectApplication/project_core/templatetags/get_dictionary_value.py
+++ ProjectApplication/project_core/templatetags/get_dictionary_value.py
@@ -3,5 +3,4 @@ from django.template.defaulttags import register
 
 @register.filter
 def get_dictionary_value(dictionary, key):
-    print('Key:', key, 'Value:', dictionary[key])
     return dictionary[key]
