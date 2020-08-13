a43dab4e99c93f76d922395568a5a1ce56e3a5d6
Carles Pina i Estany <carles@pina.cat>
2020-08-13 15:05:44 +0200

    Fix bug where the list of open calls in /calls/ was not correct.
    The bug happened when a call became open (or closed) after the
    application start time.
    
    For the reason and the fix see the comment in the code.

--- ProjectApplication/project_core/views/external/call.py
+++ ProjectApplication/project_core/views/external/call.py
@@ -7,9 +7,24 @@ class CallList(ListView):
     template_name = 'external/call-list.tmpl'
     context_object_name = 'calls'
     model = Call
-    queryset = Call.open_calls()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
         return context
+
+    def get_queryset(self):
+        # Avoid using ListView.queryset. Call.open_calls() uses timezone.now().
+        # If using ListView.queryset the queryset is retreived on the application startup
+        # so calls that become open during the application startup would not appear here
+        # (or calls that become closed after the application startup would not disappear).
+        #
+        # Another solution was to do the query in call.open_calls() like:
+        #         from django.db.models.functions import Now
+        #         return Call.objects.filter(call_open_date__lte=Now(),
+        #                                    submission_deadline__gte=Now())
+        # in this case the query has the variable CURRENT_TIMESTAMP and it works.
+        # I chose to not use this method to avoid timezones problems between Django-MariaDB and
+        # different deployments environment (e.g. Docker, system's timezone, etc.).
+        #
+        return Call.open_calls()
