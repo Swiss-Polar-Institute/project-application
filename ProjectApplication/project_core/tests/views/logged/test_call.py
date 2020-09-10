3f2781d38eeba3c5285416a58f488b3ed44d5590
Carles Pina i Estany <carles@pina.cat>
2020-09-10 11:44:57 +0100

    Add unit tests

--- ProjectApplication/project_core/tests/views/logged/test_call.py
+++ ProjectApplication/project_core/tests/views/logged/test_call.py
@@ -28,3 +28,27 @@ class CallList(TestCase):
     def test_load_create_call_add(self):
         response = self._client_management.get(reverse('logged-call-add'))
         self.assertEqual(response.status_code, 200)
+
+
+class ProposalList(TestCase):
+    def setUp(self):
+        self._proposal = database_population.create_proposal()
+        self._client_management = database_population.create_management_logged_client()
+
+    def test_list_of_proposals_for_call(self):
+        response = self._client_management.get(
+            reverse('logged-call-list-proposals', kwargs={'call_id': self._proposal.call.id}))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self._proposal.title)
+
+
+class ProposalDetail(TestCase):
+    def setUp(self):
+        self._proposal = database_population.create_proposal()
+        self._client_management = database_population.create_management_logged_client()
+
+    def test_proposal_detail(self):
+        response = self._client_management.get(
+            reverse('logged-call-proposal-detail', kwargs={'pk': self._proposal.id}))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self._proposal.title)
