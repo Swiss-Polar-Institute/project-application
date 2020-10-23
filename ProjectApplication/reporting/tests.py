fd96ab7c6caaf1e3d9298d2a4c0c0ece2f2f9ba7
Carles Pina i Estany <carles@pina.cat>
2020-10-23 11:33:46 +0100

    add basic unit test for the reporting (checks that "get" succeeds with HTTP 200)

--- ProjectApplication/reporting/tests.py
+++ ProjectApplication/reporting/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from project_core.tests import database_population
+
+
+class ReportingTest(TestCase):
+    def setUp(self):
+        self._project = database_population.create_project()
+        self._client_management = database_population.create_management_logged_client()
+
+    def test_get(self):
+        response = self._client_management.get(reverse('logged-reporting'))
+        self.assertEqual(response.status_code, 200)
