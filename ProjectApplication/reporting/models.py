9bb20620e342539ab78d22a030473be7864384a3
Carles Pina i Estany <carles@pina.cat>
2020-10-23 10:58:35 +0100

    Make description a CharField not TextField. It will be displayed in a way that makes sense to be shorter

--- ProjectApplication/reporting/models.py
+++ ProjectApplication/reporting/models.py
@@ -6,7 +6,8 @@ from project_core.models import FundingInstrument
 class FundingInstrumentYearMissingData(models.Model):
     funding_instrument = models.ForeignKey(FundingInstrument, on_delete=models.PROTECT)
     finance_year = models.IntegerField()
-    description = models.TextField(help_text='Reason that there is missing data. It might be shown in the management')
+    description = models.CharField(help_text='Reason that there is missing data. It might be shown in the management',
+                                   max_length=128)
 
     def __str__(self):
         return f'{self.funding_instrument}-{self.finance_year}'
