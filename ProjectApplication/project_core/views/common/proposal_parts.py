c27f612b7f35be800593704974c71d9627720d3c
Carles Pina i Estany <carles@pina.cat>
2021-01-14 15:33:57 +0000

    Fixed bug: use the correct variaable
    TODO: unit tests

--- ProjectApplication/project_core/views/common/proposal_parts.py
+++ ProjectApplication/project_core/views/common/proposal_parts.py
@@ -15,16 +15,15 @@ class ProposalParts:
             # It needs either the proposal or the call to find the questions
             assert False, 'In order to find the questions: pass either the Proposal or the Call'
 
-        for part in call.parts():
+        for part in self._call.parts():
             part.questions_form = Questions(post, files,
                                             proposal=proposal,
-                                            call=call,
+                                            call=self._call,
                                             call_part=part,
                                             prefix=ProposalParts._form_prefix(part))
 
             part.questions_answers_text = part.questions_form.questions_answers_text()
             part.questions_answers_file = part.questions_form.questions_answers_file()
-
 
             self._parts.append(part)
 
