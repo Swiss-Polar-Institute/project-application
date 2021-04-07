f47598727cd467a7028f1ba50c7566f9b940871e
Carles Pina i Estany <carles@pina.cat>
2021-04-07 11:36:13 +0100

    Updates to Django 3.2

--- ProjectApplication/ProjectApplication/settings.py
+++ ProjectApplication/ProjectApplication/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Application definition
 
