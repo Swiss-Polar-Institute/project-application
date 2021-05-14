eef9050a3e10e9a1dc94a5b6389304594095ffe8
Carles Pina i Estany <carles@pina.cat>
2021-05-14 11:57:30 +0100

    Add search_fields in the admin for comments

--- ProjectApplication/colours/admin.py
+++ ProjectApplication/colours/admin.py
@@ -5,12 +5,14 @@ from .models import Colour, ColourPair
 
 
 class ColourAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'hex_code', )
     list_display = ('name', 'hex_code', )
     ordering = ('name', 'hex_code', )
     form = ColourForm
 
 
 class ColourPairAdmin(admin.ModelAdmin):
+    search_fields = ('description', 'background__name', 'text__name', )
     list_display = ('description', 'background', 'text')
     ordering = ('description', )
 
--- ProjectApplication/comments/admin.py
+++ ProjectApplication/comments/admin.py
@@ -4,127 +4,151 @@ import comments.models
 
 
 class ProposalCommentAdmin(admin.ModelAdmin):
+    search_fields = ('proposal__title', 'text', )
     list_display = ('proposal', 'text', 'created_by', 'created_on',)
-    ordering = ('proposal', 'text', 'created_on', 'modified_on', )
+    ordering = ('proposal', 'text', 'created_on', 'modified_on',)
 
 
 class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'colour__description',)
     list_display = ('name', 'colour', 'created_on',)
     ordering = ('name', 'colour', 'created_on',)
 
 
 class ProposalCommentCategoryAdmin(admin.ModelAdmin):
+    search_fields = ('category__name', )
     list_display = ('category', 'created_on',)
     ordering = ('category', 'created_on',)
 
 
 class ProposalAttachmentCategoryAdmin(admin.ModelAdmin):
+    search_fields = ('category__name', )
     list_display = ('category', 'created_on',)
     ordering = ('category', 'created_on',)
 
 
 class ProposalAttachmentAdmin(admin.ModelAdmin):
+    search_fields = ('proposal__title', 'file', )
     list_display = ('proposal', 'file', 'created_by', 'created_on',)
     ordering = ('proposal', 'file', 'created_by', 'created_on',)
 
 
 class CallAttachmentCategoryAdmin(admin.ModelAdmin):
+    search_fields = ('category__name',)
     list_display = ('category', 'created_on',)
     ordering = ('category', 'created_on',)
 
 
 class CallAttachmentAdmin(admin.ModelAdmin):
+    search_fields = ('call__long_name', 'file',)
     list_display = ('call', 'file', 'created_by', 'created_on',)
     ordering = ('call', 'file', 'created_by', 'created_on',)
-    search_fields = ('call__long_name',)
 
 
 class CallCommentCategoryAdmin(admin.ModelAdmin):
+    search_fields = ('category__name',)
     list_display = ('category', 'created_on',)
     ordering = ('category', 'created_on',)
 
 
 class CallCommentAdmin(admin.ModelAdmin):
+    search_fields = ('call__long_name', 'text',)
     list_display = ('call', 'text', 'created_by', 'created_on',)
-    ordering = ('call', 'text', 'created_on', 'modified_on', )
+    ordering = ('call', 'text', 'created_on', 'modified_on',)
 
 
 class ProposalEvaluationAttachmentCategoryAdmin(admin.ModelAdmin):
+    search_fields = ('category__name', )
     list_display = ('category', 'created_on',)
     ordering = ('category', 'created_on',)
 
 
 class ProposalEvaluationAttachmentAdmin(admin.ModelAdmin):
+    search_fields = ('proposal_evaluation__proposal__title', 'file', )
     list_display = ('proposal_evaluation', 'file', 'created_by', 'created_on',)
     ordering = ('proposal_evaluation', 'file', 'created_by', 'created_on',)
 
 
 class ProposalEvaluationCommentCategoryAdmin(admin.ModelAdmin):
+    search_fields = ('category__name', )
     list_display = ('category', 'created_on',)
     ordering = ('category', 'created_on',)
 
 
 class ProposalEvaluationCommentAdmin(admin.ModelAdmin):
+    search_fields = ('proposal_evaluation__proposal__title', 'text', )
     list_display = ('proposal_evaluation', 'text', 'created_by', 'created_on')
     ordering = ('proposal_evaluation', 'text', 'created_by', 'created_on')
 
 
 class CallEvaluationCommentCategoryAdmin(admin.ModelAdmin):
+    search_fields = ('category__name',)
     list_display = ('category', 'created_on',)
     ordering = ('category', 'created_on',)
 
 
 class CallEvaluationCommentAdmin(admin.ModelAdmin):
+    search_fields = ('call_evaluation__call__long_name', 'text',)
     list_display = ('call_evaluation', 'text', 'created_by', 'created_on')
     ordering = ('call_evaluation', 'text', 'created_by', 'created_on')
 
 
 class ProjectAttachmentCategoryAdmin(admin.ModelAdmin):
+    search_fields = ('category__name', )
     list_display = ('category', 'created_on',)
     ordering = ('category', 'created_on',)
 
 
 class ProjectAttachmentAdmin(admin.ModelAdmin):
+    search_fields = ('project__title', 'file', )
     list_display = ('project', 'file', 'created_by', 'created_on',)
     ordering = ('project', 'file', 'created_by', 'created_on',)
 
 
 class ProjectCommentCategoryAdmin(admin.ModelAdmin):
+    search_fields = ('category__name', )
     list_display = ('category', 'created_on',)
     ordering = ('category', 'created_on',)
 
 
 class ProjectCommentAdmin(admin.ModelAdmin):
+    search_fields = ('project__title', 'text', )
     list_display = ('project', 'text', 'created_by', 'created_on')
     ordering = ('project', 'text', 'created_by', 'created_on')
 
 
 class InvoiceCommentCategoryAdmin(admin.ModelAdmin):
+    search_fields = ('category__name', )
     list_display = ('category', 'created_on')
     ordering = ('category', 'created_on')
 
 
 class InvoiceCommentAdmin(admin.ModelAdmin):
+    search_fields = ('invoice__id', 'invoice__amount', 'text', )
     list_display = ('invoice', 'text', 'created_by', 'created_on')
     ordering = ('invoice', 'text', 'created_by', 'created_on')
 
 
 class GrantAgreementAttachmentCategoryAdmin(admin.ModelAdmin):
+    search_fields = ('category__name',)
     list_display = ('category', 'created_on',)
     ordering = ('category', 'created_on',)
 
 
 class GrantAgreementAttachmentAdmin(admin.ModelAdmin):
+    search_fields = ('grant_agreement__project__title', 'file',)
     list_display = ('grant_agreement', 'file', 'created_by', 'created_on',)
     ordering = ('grant_agreement', 'file', 'created_by', 'created_on',)
 
 
 class GrantAgreementCommentCategoryAdmin(admin.ModelAdmin):
+    search_fields = ('category__name',)
     list_display = ('category', 'created_on',)
     ordering = ('category', 'created_on',)
 
 
 class GrantAgreementCommentAdmin(admin.ModelAdmin):
+    search_fields = ('grant_agreement__project__title', 'text',)
     list_display = ('grant_agreement', 'text', 'created_by', 'created_on')
     ordering = ('grant_agreement', 'text', 'created_by', 'created_on')
 
