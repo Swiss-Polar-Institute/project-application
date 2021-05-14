89387c1ad57753773bbc9cf8cde7042150607103
Carles Pina i Estany <carles@pina.cat>
2021-05-14 10:31:54 +0100

    Add unit test for FundingInstrumentYearMissingData

--- ProjectApplication/reporting/tests.py
+++ ProjectApplication/reporting/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 from project_core.tests import database_population
+from reporting.models import FundingInstrumentYearMissingData
 
 
 class ReportingTest(TestCase):
@@ -12,3 +13,34 @@ class ReportingTest(TestCase):
     def test_get(self):
         response = self._client_management.get(reverse('logged-reporting'))
         self.assertEqual(response.status_code, 200)
+
+
+class FundingInstrumentYearMissingDataModel(TestCase):
+    def setUp(self):
+        self._funding_instrument = database_population.create_funding_instrument()
+
+    def test_is_missing_data_false(self):
+        funding_instrument_year_missing_data = FundingInstrumentYearMissingData()
+
+        self.assertFalse(FundingInstrumentYearMissingData.is_missing_data(
+            FundingInstrumentYearMissingData.MissingDataType.CAREER_STAGE_PROPOSAL_APPLICANT)[0])
+
+    def test_is_missing_data_true(self):
+        funding_instrument_year_missing_data = FundingInstrumentYearMissingData()
+        funding_instrument_year_missing_data.missing_data_type = FundingInstrumentYearMissingData.MissingDataType.CAREER_STAGE_PROPOSAL_APPLICANT
+        funding_instrument_year_missing_data.save()
+
+        self.assertTrue(FundingInstrumentYearMissingData.is_missing_data(
+            FundingInstrumentYearMissingData.MissingDataType.CAREER_STAGE_PROPOSAL_APPLICANT)[0])
+
+    def test_is_missing_data_true_only_one_year(self):
+        funding_instrument_year_missing_data = FundingInstrumentYearMissingData()
+        funding_instrument_year_missing_data.missing_data_type = FundingInstrumentYearMissingData.MissingDataType.CAREER_STAGE_PROPOSAL_APPLICANT
+        funding_instrument_year_missing_data.finance_year = 2016
+        funding_instrument_year_missing_data.save()
+
+        self.assertFalse(FundingInstrumentYearMissingData.is_missing_data(
+            FundingInstrumentYearMissingData.MissingDataType.CAREER_STAGE_PROPOSAL_APPLICANT, year=2017)[0])
+
+        self.assertTrue(FundingInstrumentYearMissingData.is_missing_data(
+            FundingInstrumentYearMissingData.MissingDataType.CAREER_STAGE_PROPOSAL_APPLICANT, year=2016)[0])
