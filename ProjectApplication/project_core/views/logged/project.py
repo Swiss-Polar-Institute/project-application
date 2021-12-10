ff194142b583bbdfe2052bf6edb6921067c645c7
Carles Pina i Estany <carles@pina.cat>
2021-12-10 16:06:49 +0100

    Highlight the appropiate header

--- ProjectApplication/project_core/views/logged/project.py
+++ ProjectApplication/project_core/views/logged/project.py
@@ -132,7 +132,7 @@ class UserListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        context.update({'active_section': 'users',
+        context.update({'active_section': 'lists',
                         'active_subsection': 'user-list',
                         'sidebar_template': 'logged/_sidebar-lists.tmpl'})
 
