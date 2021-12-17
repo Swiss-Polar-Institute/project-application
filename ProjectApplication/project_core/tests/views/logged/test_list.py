208c7d7e657ea14a1661d77c9da30d6c5ae19603
Carles Pina i Estany <carles@pina.cat>
2021-12-17 13:55:48 +0000

    Add new test

---
+++ ProjectApplication/project_core/tests/views/logged/test_list.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from project_core.tests import database_population
+
+
+class CallFormTest(TestCase):
+    def test_homepage(self):
+        client = database_population.create_management_logged_client()
+
+        response = client.get(reverse('logged-lists'))
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, 'List Proposals')
+        self.assertContains(response, 'List Projects')
+        self.assertContains(response, 'List People')
+        self.assertContains(response, 'List Financial keys')
+        self.assertContains(response, 'List Users')
