646846002ff0ee829cfabdc72b6671a70cfee773
Carles Pina i Estany <carles@pina.cat>
2021-12-17 14:11:04 +0000

    Add TestUser (Detail and List only)

---
+++ ProjectApplication/project_core/tests/views/logged/test_user.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+
+from project_core.tests import database_population
+
+
+class TestUserList(TestCase):
+    def setUp(self):
+        self._user = database_population.create_management_user()
+        self._client_management = database_population.create_management_logged_client()
+
+    def test_get(self):
+        response = self._client_management.get(reverse('logged-user-list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self._user.username)
+        self.assertContains(response, 'Management')
+
+
+class TestUserDetail(TestCase):
+    def setUp(self):
+        self._user = database_population.create_management_user()
+        self._client_management = database_population.create_management_logged_client()
+
+    def test_get(self):
+        response = self._client_management.get(reverse('logged-user-detail'))
+        self.assertEqual(response, self._user.username)
+        self.assertContains(response, self._user.username)
