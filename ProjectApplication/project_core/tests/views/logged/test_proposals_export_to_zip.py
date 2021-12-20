b341ec9a65a5701932bb30ecad182c86d0c5d044
Carles Pina i Estany <carles@pina.cat>
2021-12-20 15:52:42 +0000

    Add basic Proposal to Zip test

--- ProjectApplication/project_core/tests/views/logged/test_proposals_export_to_zip.py
+++ ProjectApplication/project_core/tests/views/logged/test_proposals_export_to_zip.py
@@ -1,6 +1,23 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import TestCase
+from django.urls import reverse
+
+from project_core.tests import database_population
 
 
-class ProposalsExportZip(TestCase):
+class ProposalsExportZip(StaticLiveServerTestCase):
+    port = 9998
+
     def setUp(self):
-        pass
+        self._proposal = database_population.create_proposal()
+        self._client_management = database_population.create_management_logged_client()
+
+    def test_get_ok(self):
+        response = self._client_management.get(
+            reverse('proposal-detail-zip',
+                    kwargs={'uuid': self._proposal.uuid})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response['content-disposition'].endswith('.zip"'))
+        self.assertTrue(response['content-disposition'].startswith('attachment; filename="'))
+        self.assertGreaterEqual(int(response['content-length']), 98000)
