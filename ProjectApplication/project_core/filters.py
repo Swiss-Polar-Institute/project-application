96aa75352a74fb594c58801c4fb580a657097221
Marc-Antoine Cloux <marc-antoine@darwindigital.com>
2022-01-20 18:16:38 +0100

    Add more filters

--- ProjectApplication/project_core/filters.py
+++ ProjectApplication/project_core/filters.py
@@ -6,21 +6,23 @@ from project_core.models import Project
 
 
 class ProjectFilterSet(FilterSet):
-    key_title = CharFilter(method='filter_key_title', label="Key or Title")
-    keywords = CharFilter(method='filter_keywords')
+    key_title = CharFilter(method='filter_any', label="Key, Title, Keyword, PI, laysummary or organisation")
     geographical_areas = CharFilter(method='filter_geographical_areas')
     funding_instrument = CharFilter(method='filter_funding_instrument')
     start_date = CharFilter(method='filter_start_date')
 
-    def filter_key_title(self, queryset, name, value):
+    def filter_any(self, queryset, name, value):
         search_query = Q(
             Q(key__icontains=value) |
-            Q(title__icontains=value)
+            Q(title__icontains=value) |
+            Q(keywords__name__icontains=value) |
+            Q(principal_investigator__person__first_name__icontains=value) |
+            Q(principal_investigator__person__surname__icontains=value) |
+            Q(laysummary__text__icontains=value) |
+            Q(principal_investigator__organisation_names__name__icontains=value)
+
         )
         return queryset.filter(search_query)
-
-    def filter_keywords(self, queryset, name, value):
-        return queryset.filter(keywords__name=value)
 
     def filter_geographical_areas(self, queryset, name, value):
         return queryset.filter(geographical_areas__name=value)
@@ -33,4 +35,4 @@ class ProjectFilterSet(FilterSet):
 
     class Meta:
         model = Project
-        fields = ('key_title', 'keywords', 'geographical_areas', 'funding_instrument', 'start_date')
+        fields = ('key_title', 'geographical_areas', 'funding_instrument', 'start_date')
