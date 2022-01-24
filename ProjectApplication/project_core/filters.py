ae69a9b71e491456f6b1f8d0d24409771457397e
Marc-Antoine Cloux <marc-antoine@darwindigital.com>
2022-01-24 14:02:58 +0100

    Update filter function

--- ProjectApplication/project_core/filters.py
+++ ProjectApplication/project_core/filters.py
@@ -1,28 +1,29 @@
-from django.db.models import Q
+from functools import reduce
+from operator import or_
 
+from django.db.models import Q
 from django_filters.rest_framework import CharFilter, FilterSet
 
 from project_core.models import Project
 
 
 class ProjectFilterSet(FilterSet):
-    key_title = CharFilter(method='filter_any', label="Key, Title, Keyword, PI, laysummary or organisation")
+    key_any = CharFilter(method='filter_any', label="Key, Title, Keyword, PI, Lay summary or Organisation")
     geographical_areas = CharFilter(method='filter_geographical_areas')
     funding_instrument = CharFilter(method='filter_funding_instrument')
     start_date = CharFilter(method='filter_start_date')
 
     def filter_any(self, queryset, name, value):
-        search_query = Q(
-            Q(key__icontains=value) |
-            Q(title__icontains=value) |
-            Q(keywords__name__icontains=value) |
-            Q(principal_investigator__person__first_name__icontains=value) |
-            Q(principal_investigator__person__surname__icontains=value) |
-            Q(laysummary__text__icontains=value) |
-            Q(principal_investigator__organisation_names__name__icontains=value)
+        fields = [
+            "key", "title", "keywords__name",
+            "principal_investigator__person__first_name",
+            "principal_investigator__person__surname",
+            "laysummary__text", "principal_investigator__organisation_names__name"
+        ]
+        q_field = reduce(or_, (Q(**{f"{field}__icontains": word}) for word in value.split() for field in fields))
+        print(queryset.filter(q_field))
 
-        )
-        return queryset.filter(search_query)
+        return queryset.filter(q_field).distinct()
 
     def filter_geographical_areas(self, queryset, name, value):
         return queryset.filter(geographical_areas__name=value)
@@ -35,4 +36,4 @@ class ProjectFilterSet(FilterSet):
 
     class Meta:
         model = Project
-        fields = ('key_title', 'geographical_areas', 'funding_instrument', 'start_date')
+        fields = ('key_any', 'geographical_areas', 'funding_instrument', 'start_date')
