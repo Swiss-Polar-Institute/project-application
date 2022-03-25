baeae817e0eb7ccb8d0c36bc6278b55a3e2ba27b
Carles Pina i Estany <carles@pina.cat>
2022-03-25 18:24:37 +0000

    fix: script to upload web img

--- ProjectApplication/update_image_for_web.py
+++ ProjectApplication/update_image_for_web.py
@@ -12,10 +12,7 @@ from ProjectApplication import settings
 def run():
     for object in Medium.objects.all():
         if not object.file_web:
-            url = f"{settings.AWS_S3_ENDPOINT_URL}/{settings.AWS_STORAGE_BUCKET_NAME}/{settings.AWS_LOCATION}/{object.file}"
-            response = requests.get(url)
-            if response.status_code == 200:
-                object.save()
+            object.save()
 
 
 if __name__ == '__main__':
