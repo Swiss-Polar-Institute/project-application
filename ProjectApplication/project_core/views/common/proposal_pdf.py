76fc22cdeeb5717eafef64d1543678d676e9d79b
Marc-Antoine Cloux <marc-antoine@darwindigital.com>
2022-05-25 08:47:07 +0200

    fix: pdf generation and put in create_pdf_for_proposal function

--- ProjectApplication/project_core/views/common/proposal_pdf.py
+++ ProjectApplication/project_core/views/common/proposal_pdf.py
@@ -18,43 +18,24 @@ def create_pdf_for_proposal(proposal, request):
     if url.startswith('http://testserver/'):
         url = url.replace('http://testserver/', 'http://localhost:9998/', 1)
 
-    command = ['wkhtmltopdf', '--quiet']
+    options = {
+        'page-size': 'A4',
+        'margin-top': '0.75in',
+        'margin-right': '0.75in',
+        'margin-bottom': '0.95in',
+        'margin-left': '0.75in',
+        'encoding': "UTF-8",
+        'minimum-font-size': "12"
+    }
 
-    if settings.SELF_HTTP_USERNAME:
-        command += ['--username', settings.SELF_HTTP_USERNAME, '--password', settings.SELF_HTTP_PASSWORD]
-
-    command += [url, '-']
-
-    process = subprocess.run(command,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
-
-    if process.returncode != 0:
-        logger.warning(f'NOTIFY: PDF generation warning for {proposal.pk}: {process.stderr}')
-
-    return process.stdout
+    return pdfkit.from_url(url, options=options, verbose=True)
 
 
 class ProposalDetailViewPdf(View):
     def get(self, request, *args, **kwargs):
         proposal = Proposal.objects.get(uuid=kwargs['uuid'])
-        url = reverse('proposal-detail', kwargs={'uuid': proposal.uuid})
-        url = request.build_absolute_uri(url)
 
-        if url.startswith('http://testserver/'):
-            url = url.replace('http://testserver/', 'http://localhost:9998/', 1)
-
-        options = {
-            'page-size': 'A4',
-            'margin-top': '0.75in',
-            'margin-right': '0.75in',
-            'margin-bottom': '0.95in',
-            'margin-left': '0.75in',
-            'encoding': "UTF-8",
-            'minimum-font-size': "12"
-        }
-
-        proposal_pdf = pdfkit.from_url(url, options=options, verbose=True)
+        proposal_pdf = create_pdf_for_proposal(proposal, request)
 
         response = HttpResponse(content_type='application/pdf')
 
