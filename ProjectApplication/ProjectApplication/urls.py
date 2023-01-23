a6dc88050f81dfe455226800315c0fd28caeb54d
Marc-Antoine Cloux <marc-antoine@darwindigital.com>
2023-01-23 14:12:46 +0100

    update: add OTP login

--- ProjectApplication/ProjectApplication/urls.py
+++ ProjectApplication/ProjectApplication/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django_otp.admin import OTPAdminSite
 
 from ckeditor_uploader import views as ckeditor_views
+
+
+admin.site.__class__ = OTPAdminSite
 
 urlpatterns = [
     path('', include('project_core.urls')),
